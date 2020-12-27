41e27136f771dd627543d08f608d9e56a666c71a
Anna Clemens <git@annaclemens.io>
2020-12-27 11:13:14 -0500

    chore: initial commit

---
+++ DalamudPackager/DalamudPackager.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace DalamudPackager {
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+    [SuppressMessage("ReSharper", "UnusedType.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    public class DalamudPackager : Task {
+        /// <summary>
+        /// Set this to $(AssemblyName)
+        /// </summary>
+        [Required]
+        public string AssemblyName { get; set; } = null!;
+
+        /// <summary>
+        /// Set this to $(ProjectDir)
+        /// </summary>
+        [Required]
+        public string ProjectDir { get; set; } = null!;
+
+        /// <summary>
+        /// Set this to $(OutputPath)
+        /// </summary>
+        [Required]
+        public string OutputPath { get; set; } = null!;
+
+        /// <summary>
+        /// This can be either "json" or "yaml"
+        /// </summary>
+        public string ManifestType { get; set; } = "json";
+
+        private ManifestKind RealManifestType => this.ManifestType switch {
+            "json" => ManifestKind.Json,
+            "yaml" => ManifestKind.Yaml,
+            _ => throw new ArgumentException("Invalid manifest type: expected either 'json' or 'yaml'", nameof(this.ManifestType)),
+        };
+
+        public byte VersionComponents { get; set; } = 4;
+
+        public bool MakeZip { get; set; } = false;
+
+        public string? Exclude { get; set; }
+
+        public string? Include { get; set; }
+
+        private List<string> ExcludeFiles => StringToList(this.Exclude);
+
+        private List<string> IncludeFiles => StringToList(this.Include);
+
+        public override bool Execute() {
+            // load the manifest from the source file
+            var manifest = this.LoadManifest();
+
+            // verify required fields on the manifest
+            if (manifest.LogMissing(this.Log)) {
+                return false;
+            }
+
+            // set some things automatically from the assembly
+            var assemblyName = this.LoadAssemblyInfo();
+            manifest.SetProperties(assemblyName, this.VersionComponents);
+
+            // save the json manifest in the output
+            this.SaveManifest(manifest);
+
+            // make a zip if specified
+            return !this.MakeZip || this.CreateZip();
+        }
+
+        private bool CreateZip() {
+            // get path of the folder called the assembly name where we'll have the manifest and latest.zip
+            var zipOutput = Path.Combine(this.OutputPath, this.AssemblyName);
+
+            // remove the output folder if it exists
+            if (Directory.Exists(zipOutput)) {
+                Directory.Delete(zipOutput, true);
+            }
+
+            // normalise file lists
+            this.NormaliseFiles();
+
+            // determine file names to zip
+            var includeLen = this.IncludeFiles.Count;
+            var excludeLen = this.ExcludeFiles.Count;
+
+            if (includeLen > 0 && excludeLen > 0) {
+                this.Log.LogError("Specify either Include of Exclude on your DalamudPackager task, not both.");
+                return false;
+            }
+
+            string[] fileNames;
+
+            if (includeLen == 0 && excludeLen == 0) {
+                fileNames = Directory.GetFiles(this.OutputPath)
+                    .Select(file => file.Substring(this.OutputPath.Length).TrimStart('/', '\\'))
+                    .ToArray();
+            } else if (includeLen > 0) {
+                fileNames = this.IncludeFiles.ToArray();
+            } else {
+                fileNames = Directory.GetFiles(this.OutputPath)
+                    .Select(file => file.Substring(this.OutputPath.Length).Replace('/', '\\').TrimStart('\\'))
+                    .Where(file => !this.ExcludeFiles.Contains(file))
+                    .ToArray();
+            }
+
+            // create zip of files in the output path
+            var zipPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            using (var zipFile = File.Create(zipPath)) {
+                using (var zip = new ZipArchive(zipFile, ZipArchiveMode.Create)) {
+                    foreach (var file in fileNames) {
+                        var filePath = Path.Combine(this.OutputPath, file);
+                        zip.CreateEntryFromFile(filePath, file);
+                    }
+                }
+            }
+
+            // create the output folder
+            Directory.CreateDirectory(zipOutput);
+
+            // copy manifest to output
+            File.Copy(
+                Path.Combine(this.OutputPath, $"{this.AssemblyName}.json"),
+                Path.Combine(zipOutput, $"{this.AssemblyName}.json")
+            );
+
+            // move zip to output
+            File.Move(
+                zipPath,
+                Path.Combine(zipOutput, "latest.zip")
+            );
+
+            return true;
+        }
+
+        private AssemblyName LoadAssemblyInfo() {
+            var assemblyPath = Path.Combine(this.OutputPath, $"{this.AssemblyName}.dll");
+            var fullPath = Path.GetFullPath(assemblyPath);
+
+            return System.Reflection.AssemblyName.GetAssemblyName(fullPath);
+        }
+
+        private Manifest LoadManifest() {
+            var ext = this.RealManifestType switch {
+                ManifestKind.Json => "json",
+                ManifestKind.Yaml => "yaml",
+                _ => throw new ArgumentOutOfRangeException($"extension doesn't exist for {this.RealManifestType}"),
+            };
+
+            var manifestPath = Path.Combine(this.ProjectDir, $"{this.AssemblyName}.{ext}");
+
+            using var manifestFile = File.Open(manifestPath, FileMode.Open);
+            using var manifestStream = new StreamReader(manifestFile);
+
+            return this.RealManifestType switch {
+                ManifestKind.Json => LoadJsonManifest(manifestStream),
+                ManifestKind.Yaml => LoadYamlManifest(manifestStream),
+                _ => throw new Exception("unreachable"),
+            };
+        }
+
+        private static Manifest LoadYamlManifest(TextReader reader) {
+            var yamlDeserialiser = new DeserializerBuilder()
+                .IgnoreUnmatchedProperties()
+                .WithNamingConvention(UnderscoredNamingConvention.Instance)
+                .Build();
+
+            return yamlDeserialiser.Deserialize<Manifest>(reader);
+        }
+
+        private static Manifest LoadJsonManifest(TextReader reader) {
+            return JsonSerializer.CreateDefault().Deserialize<Manifest>(new JsonTextReader(reader))!;
+        }
+
+        private void SaveManifest(Manifest manifest) {
+            var jsonPath = Path.Combine(this.OutputPath, $"{this.AssemblyName}.json");
+
+            var jsonSerialiser = JsonSerializer.Create(new JsonSerializerSettings {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+            });
+
+            using var jsonFile = File.Open(jsonPath, FileMode.Create);
+            using var jsonStream = new StreamWriter(jsonFile);
+            jsonSerialiser.Serialize(jsonStream, manifest);
+        }
+
+        private void NormaliseFiles() {
+            this.Include = this.Include?.Replace('/', '\\');
+            this.Exclude = this.Exclude?.Replace('/', '\\');
+        }
+
+        private static List<string> StringToList(string? s) {
+            if (s == null) {
+                return new List<string>();
+            }
+
+            return s.Split(';')
+                .Select(name => name.Trim())
+                .ToList();
+        }
+    }
+
+    public enum ManifestKind {
+        /// <summary>
+        /// Superior manifest type. Easier for human consumption. Will be converted into JSON for machine consumption
+        /// during the build process.
+        /// </summary>
+        Yaml,
+
+        /// <summary>
+        /// Inferior manifest type. Not meant for human consumption.
+        /// </summary>
+        Json,
+    }
+
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+    public class Manifest {
+        /// <summary>
+        /// The author/s of the plugin.
+        /// </summary>
+        public string? Author { get; set; }
+
+        /// <summary>
+        /// The public name of the plugin.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// The internal name of the plugin, which should match the assembly name of the plugin.
+        /// </summary>
+        public string? InternalName { get; set; }
+
+        /// <summary>
+        /// The current assembly version of the plugin.
+        /// </summary>
+        public string? AssemblyVersion { get; set; }
+
+        /// <summary>
+        /// A description of the plugins functions.
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// The version of the game this plugin works with.
+        /// </summary>
+        public string ApplicableVersion { get; set; } = "any";
+
+        /// <summary>
+        /// An URL to the website or source code of the plugin.
+        /// </summary>
+        public string? RepoUrl { get; set; }
+
+        /// <summary>
+        /// List of tags defined on the plugin.
+        /// </summary>
+        public List<string>? Tags { get; set; }
+
+        /// <summary>
+        /// The API level of this plugin.
+        /// </summary>
+        public int DalamudApiLevel { get; set; } = 2;
+
+        internal bool LogMissing(TaskLoggingHelper log) {
+            var anyNull = this.Name == null || this.Author == null || this.Description == null;
+
+            if (this.Name == null) {
+                log.LogError("Plugin name is required in your manifest.");
+            }
+
+            if (this.Author == null) {
+                log.LogError("Author name is required in your plugin manifest.");
+            }
+
+            if (this.Description == null) {
+                log.LogError("Description is required in your plugin manifest.");
+            }
+
+            return anyNull;
+        }
+
+        internal void SetProperties(AssemblyName assembly, byte components) {
+            this.AssemblyVersion = assembly.Version!.ToString(components);
+            this.InternalName = assembly.Name;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,88 @@
+﻿# DalamudPackager
+
+This is an MSBuild task that is designed to simplify creating plugins for [Dalamud][dalamud].
+
+Install the NuGet package `DalamudPackager`. When you build in release mode, a folder will be placed in your output directory
+containing your plugin manifest and `latest.zip`.
+
+If you need to configure the task, create a file called `DalamudPackager.targets` in your project and use the template
+below.
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<Project>
+    <Target Name="PackagePlugin" AfterTargets="Build" Condition="'$(Configuration)' == 'Release'">
+        <DalamudPackager
+                ProjectDir="$(ProjectDir)"
+                OutputPath="$(OutputPath)"
+                AssemblyName="$(AssemblyName)"
+                MakeZip="true"/>
+    </Target>
+</Project>
+```
+
+## Manifest generation
+
+DalamudPackager reduces the amount of keys you need to include in your manifest, filling in the rest from sane defaults
+or your assembly. You can, of course, specify everything manually.
+
+In addition, DalamudPackager allows you to use YAML, a more human-friendly format, for your manifest instead of JSON.
+YAML uses **snake_case** for keys. JSON uses **PascalCase** for keys. The example below is a YAML manifest, but
+generation works the same for JSON manifests.
+
+```yaml
+name: Test Plugin
+author: You
+description: |-
+  This is a test plugin - this first line is a summary.
+
+  Down here is a more detailed explanation of what the plugin
+  does, manually wrapped to make sure it stays visible in the
+  installer.
+repo_url: https://example.com/
+```
+
+Notice how keys like `assembly_version` and `dalamud_api_level` are missing. You can include these if you'd like, but
+DalamudPackager will automatically do it for you when you build your project. If you build your project in Release mode,
+you will find the following JSON file in your output directory.
+
+```json5
+{
+  "Author": "You",
+  "Name": "Test Plugin",
+  // this will be set to your AssemblyName automatically
+  "InternalName": "TestPlugin",
+  // this will be set to your assembly's version automatically
+  "AssemblyVersion": "1.0.0.0",
+  "Description": "This is a test plugin - this first line is a summary.\n\nDown here is a more detailed explanation of what the plugin\ndoes, manually wrapped to make sure it stays visible in the\ninstaller.",
+  // this will be set to "any" automatically
+  "ApplicableVersion": "any",
+  "RepoUrl": "https://example.com/",
+  // this will be set to 2 automatically
+  "DalamudApiLevel": 2
+}
+```
+
+## `.zip` file generation
+
+DalamudPackager will also create a folder with `latest.zip` for you if `MakeZip` is `true`. Check your output directory
+for a folder with the name of your assembly. Inside will be your manifest and `latest.zip`, ready for distribution or
+PRs.
+
+Note that the entire contents of your output directory will be zipped by default. Either turn off copy local for Dalamud
+references, set up a task to clean your output directory, or use `Exclude` or `Include` if you want to change this.
+
+## Task attributes
+
+| Attribute | Description | Required | Default |
+| --------- | ----------- | -------- | ------- |
+| `ProjectDir` | This is the path where your `csproj` is located. You must set this to `$(ProjectDir)`. | **Yes** | *None* - set to `$(ProjectDir)` |
+| `OutputPath` | This is the path that your assemblies are output to after build. You must set this to `$(OutputPath)`. | **Yes** | *None* - set to `$(OutputPath)` |
+| `AssemblyName` | This is the name of the assembly that Dalamud will be loading. You used to need to specify this in your manifest as `InternalName`. | **Yes** | *None* - set to `$(AssemblyName)` |
+| `ManifestType` | You can choose between `json` and `yaml` for your manifest file. | No | `json` |
+| `VersionComponents` | How many components of the assembly's version to include in the generated manifest. If you use semantic versioning, set this to `3`. | No | `4` |
+| `MakeZip` | If this is `true`, a folder will be created in your `OutputPath` that contains your generated manifest and `latest.zip`, ready for PRing. | No | `false` |
+| `Exclude` | Files to exclude from the zip if `MakeZip` is `true`. Mutually exclusive with `Include`. Files should be separated by a semicolon (`;`) and be relative to `OutputPath`. Files do not need to exist. | No | *None* |
+| `Include` | Files to include in the zip if `MakeZip` is `true`. Mutually exclusive with `Exclude`. Files should be separated by a semicolon (`;`) and be relative to `OutputPath`. Files must exist. | No | *None* |
+
+[dalamud]: https://github.com/goatcorp/Dalamud
