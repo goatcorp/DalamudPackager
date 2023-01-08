12cd9888e8099e46218f9d235356413fa8b2629a
goat <goatsdev@protonmail.com>
2023-01-08 22:18:09 +0100

    2.1.10

--- DalamudPackager/DalamudPackager.cs
+++ DalamudPackager/DalamudPackager.cs
@@ -349,7 +349,7 @@ namespace DalamudPackager {
         /// <summary>
         /// The API level of this plugin.
         /// </summary>
-        public int DalamudApiLevel { get; set; } = 7;
+        public int DalamudApiLevel { get; set; } = 8;
 
         /// <summary>
         /// Gets the required Dalamud load step for this plugin to load. Takes precedence over LoadPriority.
